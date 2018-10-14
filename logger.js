790d5080903a55ee69e7fef5403c098bf0b0296d
Daniel <dant00300@gmail.com>
2018-10-14 20:10:04 +0900

    Add block hash info Must be tested on server

--- logger.js
+++ logger.js
@@ -2,41 +2,59 @@ var request = require("request");
 var fs = require("fs");
 
 function get_price_async() {
-  return new Promise((resolve, reject) => {
-
-    var options = {
+  var options = {
       url : "https://min-api.cryptocompare.com/data/price?fsym=XMR&tsyms=BTC,USD,JPY",
       method : "GET",
       headers : {"Content-Type" : "application/json"},
       json : true
     }
 
-    request(options, (err, res, body) => {
+  return new Promise((resolve, reject) => {
+    request(options, (err, res, data) => {
       if (err) {
         reject(err);
       }
       else {
-        resolve(body);
+        resolve(data);
       }
     })
   })
 }
 
 function get_network_stats_async() {
-  return new Promise((resolve, reject) => {
-    var options = {
-      url : "https://moneroblocks.info/api/get_stats",
-      method : "GET",
-      headers : {"Content-Type" : "application/json"},
-      json : true
-    }
+  var options = {
+    url : "https://moneroblocks.info/api/get_stats",
+    method : "GET",
+    headers : {"Content-Type" : "application/json"},
+    json : true
+  }
 
-    request(options, (err, res, body) => {
+  return new Promise((resolve, reject) => {
+    request(options, (err, res, data) => {
       if (err) {
         reject(err);
       }
       else {
-        resolve(body);
+        resolve(data);
+      }
+    })
+  })
+}
+
+function get_block_hash_async() {
+  var options = {
+    url : "http://127.0.0.1/json_rpc",
+    data : {"jsonrpc" : "2.0", "id" : 0, "method" : "get_block_count"},
+    headers : {"Content-Type" : "application/json"},
+    method : "POST"
+  }
+
+  return new Promise((resolve, reject) => {
+    request(options, (err, res, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data);
       }
     })
   })
@@ -45,11 +63,13 @@ function get_network_stats_async() {
 function update_price_json() {
   get_price_async()
   .then((data) => {
+
     fs.writeFile("price.json", JSON.stringify(data), (err) => {
       if (err) {
         console.error(err);
+      } else {
+        console.log("updated price.json");
       }
-      console.log("updated price.json");
       setTimeout(update_price_json, 10000);
     })
   })
@@ -65,8 +85,9 @@ function update_network_stats_json() {
     fs.writeFile("network_stats.json", JSON.stringify(data), (err) => {
       if (err) {
         console.error(err);
+      } else {
+        console.log("updated network_stats.json");
       }
-      console.log("updated network_stats.json");
       setTimeout(update_network_stats_json, 10000);
     })
   })
@@ -76,5 +97,21 @@ function update_network_stats_json() {
   })
 }
 
-update_price_json();
-update_network_stats_json();
+function update_blocks() {
+  get_block_hash_async()
+  .then((data) => {
+    fs.writeFile("block_hash.json", JSON.stringify(data), (err) => {
+      if (err) {
+        console.error(err);
+      } else {
+        console.log("updated block_hash.json");
+      }
+      setTimeout(update_blocks, 10000);
+    })
+  })
+  .catch((err) => {
+    console.error(err);
+  })
+}
+
+update_blocks();
--- logger.py
+++ logger.py
@@ -1,16 +1,21 @@
 import requests
 import json
 import time
+import chardet
 
 while True:
-    res = requests.post("http://127.0.0.1:8117/live_stats")
+    res = requests.post("http://192.168.100.109:18081/json_rpc")
     if res.status_code != 200:
         time.sleep(10)
-        continue
         print(res)
+        continue
 
+    '''
     with open("pool_stats.json", "w") as f:
         json.dump(res.json(), f)
 
     print("updated pool_stats.json")
-    time.sleep(10)
+    break
+    time.sleep()
+    '''
+    print(res.text)
